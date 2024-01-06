0bc7b6c2107c18b7e8fb8127c49727f732befbfd
Yang Liu <yliu1382011@gmail.com>
2024-01-06 22:42:00 +1100

    Added one more function to start command without waiting.

--- pkg/videos/common.go
+++ pkg/videos/common.go
@@ -26,6 +26,31 @@ func CheckPathExist(path string) error {
 	return nil
 }
 
+// This will only kick off the command but no waiting the command to finish...s
+func RunCommandContextNoWait(ctx context.Context, commandStr string, args []string) (int, error) {
+	cmd := exec.CommandContext(ctx, commandStr, args...)
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	stdOut, err := cmd.StdoutPipe()
+	if err != nil {
+		return -1, err
+	}
+	defer stdOut.Close()
+
+	err = cmd.Start()
+	log.Printf("Starting Process id is %v ...", cmd.Process.Pid)
+	if err != nil {
+		return -1, err
+	}
+	bytes, err := io.ReadAll(stdOut)
+	log.Println(bytes)
+	if err != nil {
+		return -1, err
+	}
+
+	return cmd.Process.Pid, nil
+}
+
 func RunCommandContext(ctx context.Context, commandStr string, args []string) error {
 	cmd := exec.CommandContext(ctx, commandStr, args...)
 	cmd.Stdin = os.Stdin
